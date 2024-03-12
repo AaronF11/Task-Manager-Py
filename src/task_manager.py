4afce173209d8272590e4b06a9f0ba1f0812d1ed
Aarón Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2024-03-11 21:52:54 -0600

    (#4) Add task manager class

---
+++ src/task_manager.py
@@ -0,0 +1,42 @@
+class TaskManager:
+    def __init__(self):
+        """
+        Initialize the TaskManager object with an empty list of tasks.
+        """
+        self.tasks = []
+
+    def add_task(self, task):
+        """
+        Add a task to the task manager.
+
+        Parameters:
+        - task (Task): The task to be added to the task manager.
+        """
+        self.tasks.append(task)
+
+    def display_tasks(self):
+        """
+        Display all tasks in the task manager.
+        """
+        for i, task in enumerate(self.tasks, 1):
+            print(f"{i}. {task}")
+
+    def mark_task_as_completed(self, index):
+        """
+        Mark a task as completed by its index.
+
+        Parameters:
+        - index (int): The index of the task to be marked as completed.
+        """
+        if 0 < index <= len(self.tasks):
+            self.tasks[index - 1].mark_as_completed()
+
+    def remove_task(self, index):
+        """
+        Remove a task from the task manager by its index.
+
+        Parameters:
+        - index (int): The index of the task to be removed.
+        """
+        if 0 < index <= len(self.tasks):
+            del self.tasks[index - 1]
